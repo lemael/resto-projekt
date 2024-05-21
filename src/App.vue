da57c4a9e44f9763890ed2c993894897e8bf8b04
Mael Fosso <tchindefossomael@yahoo.fr>
2024-05-21 15:25:17 +0200

    Make component for Sign-Up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <SignUp msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <img class="logo" alt="resto logo" src="../assets/resto_logo.jpg">
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" placeholder="Enter Name">
+    <input type="text" placeholder="Enter Email">
+    <input type="password" placeholder="Enter Password">
+    <button>Sign Up</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo {
+  width: 100px;
+}
+
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background-color: skyblue;
+  color: #fff;
+  cursor: pointer;
+
+}
+</style>
