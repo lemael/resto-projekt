d315ea21a8698d28cb0722378a6c36cb50ea5e1c
Mael Fosso <tchindefossomael@yahoo.fr>
2024-05-22 13:07:46 +0200

    Create Header for Project

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
+    </div>
+</template>
+<script>
+export default{
+    name:"HeaderPage"
+}
+</script>
+<style>
+.nav{
+    background-color: #cd5c5c;
+    overflow: hidden;
+}
+.nav a {
+    float: left;
+    color: #f2f2f2f2;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover {
+    background: #ddd;
+    color: #333;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,15 @@
 <template>
+    <Header />
     <h1>Hello user</h1>
 
 </template>
 <script>
+import Header from "./Header.vue"
 export default {
     name: "HomePage",
+    components:{
+        Header
+    },
     mounted()
     {
       let user= localStorage.getItem("user-info");
